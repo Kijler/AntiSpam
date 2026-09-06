07b7fef04e796b17164d6c6fd8979cbd7f3d438a
Kijler <contact@kijler.eu>
2026-09-06 04:39:02 +0200

    Implement blocked words and language file management
    Added methods to handle blocked words and language files.

--- src/main/java/cz/kijler/antiSpam/AntiSpam.java
+++ src/main/java/cz/kijler/antiSpam/AntiSpam.java
@@ -11,27 +11,53 @@ public class AntiSpam extends JavaPlugin {
     private static AntiSpam instance;
     private DatabaseManager databaseManager;
     private String latestVersion;
+    private AntiSpamListener antiSpamListener;
+    private File blockedWordsFile;
+    private FileConfiguration blockedWordsConfig;
 
     @Override
     public void onEnable() {
         instance = this;
+
         saveDefaultConfig();
-        ensureDefaultConfig();
-        createDefaultLanguageFiles();
-        checkAndUpdateLanguageFiles();
+        saveDefaultBlockedWords();
+        saveDefaultLanguageFiles();
 
         this.databaseManager = new DatabaseManager(this);
+        this.databaseManager.startReportPolling();
 
         new UpdateChecker(this, 118730).getVersion(version -> {
             if (!this.getDescription().getVersion().equalsIgnoreCase(version)) {
                 this.latestVersion = version;
-                getLogger().info("New version of AntiSpam plugin found: " + version);
-                getLogger().info("Download here: https://www.spigotmc.org/resources/antispam-1-17-1-21.118730/");
+                getLogger().info("--------------------------------------------------");
+                getLogger().info("[AntiSpam]");
+                getLogger().info("A new version of the plugin is available!");
+                getLogger().info("Current version: " + this.getDescription().getVersion());
+                getLogger().info("New version: " + version);
+                getLogger().info("Download: https://www.spigotmc.org/resources/antispam-with-web-panel.118730/");
+                getLogger().info("--------------------------------------------------");
             }
         });
 
-        getServer().getPluginManager().registerEvents(new AntiSpamListener(this), this);
+        this.antiSpamListener = new AntiSpamListener(this);
+        getServer().getPluginManager().registerEvents(this.antiSpamListener, this);
+
         getCommand("antispam").setExecutor(new AntiSpamCommand(this));
+
+        ChatReportCommand chatReportCommand = new ChatReportCommand(this);
+        if (getCommand("chatreport") != null) {
+            getCommand("chatreport").setExecutor(chatReportCommand);
+            getCommand("chatreport").setTabCompleter(chatReportCommand);
+        }
+        if (getCommand("reportchat") != null) {
+            getCommand("reportchat").setExecutor(chatReportCommand);
+            getCommand("reportchat").setTabCompleter(chatReportCommand);
+        }
+        if (getCommand("chatreportlogs") != null) {
+            getCommand("chatreportlogs").setExecutor(chatReportCommand);
+            getCommand("chatreportlogs").setTabCompleter(chatReportCommand);
+        }
+
         getLogger().info(getPrefix() + "Plugin Enabled");
     }
 
@@ -49,6 +75,10 @@ public class AntiSpam extends JavaPlugin {
 
     public DatabaseManager getDatabaseManager() {
         return databaseManager;
+    }
+
+    public AntiSpamListener getAntiSpamListener() {
+        return antiSpamListener;
     }
 
     public String getLatestVersion() {
@@ -74,182 +104,52 @@ public class AntiSpam extends JavaPlugin {
         return YamlConfiguration.loadConfiguration(file);
     }
 
-    private void createDefaultLanguageFiles() {
-        File languageFolder = new File(getDataFolder(), "languages");
-        if (!languageFolder.exists()) {
-            languageFolder.mkdirs();
+    public void reloadBlockedWordsConfig() {
+        if (blockedWordsFile == null) {
+            blockedWordsFile = new File(getDataFolder(), "BlockedWords.yml");
         }
-
-        File[] languageFiles = {
-                new File(languageFolder, "Locale_EN.yml"),
-                new File(languageFolder, "Locale_CZ.yml"),
-                new File(languageFolder, "Locale_DE.yml")
-        };
-
-        for (File file : languageFiles) {
-            if (!file.exists()) {
-                saveResource("languages/" + file.getName(), false);
-            }
-        }
+        blockedWordsConfig = YamlConfiguration.loadConfiguration(blockedWordsFile);
     }
 
-    private void ensureDefaultConfig() {
-        FileConfiguration config = getConfig();
-
-        if (!config.contains("language")) {
-            config.set("language", "EN");
+    public FileConfiguration getBlockedWordsConfig() {
+        if (blockedWordsConfig == null) {
+            reloadBlockedWordsConfig();
         }
-
-        if (!config.contains("database.type")) config.set("database.type", "SQLITE");
-        if (!config.contains("database.host")) config.set("database.host", "localhost");
-        if (!config.contains("database.port")) config.set("database.port", 3306);
-        if (!config.contains("database.database")) config.set("database.database", "minecraft");
-        if (!config.contains("database.username")) config.set("database.username", "root");
-        if (!config.contains("database.password")) config.set("database.password", "password");
-        if (!config.contains("database.useSSL")) config.set("database.useSSL", false);
-
-        if (!config.contains("punishment-issuer")) {
-            config.set("punishment-issuer", "AntiSpam");
-        }
-
-        if (!config.contains("block-repeated-messages")) {
-            config.set("block-repeated-messages", true);
-        }
-        if (!config.contains("delay")) {
-            config.set("delay", 3);
-        }
-        if (!config.contains("repeat-message-delay")) {
-            config.set("repeat-message-delay", 60);
-        }
-        if (!config.contains("block-repeated-characters")) {
-            config.set("block-repeated-characters", true);
-        }
-        if (!config.contains("max-repeating-characters")) {
-            config.set("max-repeating-characters", 3);
-        }
-        if (!config.contains("blocked-words")) {
-            config.set("blocked-words", new String[]{"badword1", "badword2"});
-        }
-
-        if (!config.contains("punishments.blocked-word.enabled")) config.set("punishments.blocked-word.enabled", true);
-        if (!config.contains("punishments.blocked-word.threshold")) config.set("punishments.blocked-word.threshold", 3);
-        if (!config.contains("punishments.blocked-word.command")) config.set("punishments.blocked-word.command", "tempmute %player% 1h Inappropriate behavior #%punish_id%");
-
-        if (!config.contains("punishments.spam.enabled")) config.set("punishments.spam.enabled", true);
-        if (!config.contains("punishments.spam.threshold")) config.set("punishments.spam.threshold", 5);
-        if (!config.contains("punishments.spam.command")) config.set("punishments.spam.command", "tempmute %player% 30m Spam #%punish_id%");
-
-        if (!config.contains("punishments.flood.enabled")) config.set("punishments.flood.enabled", true);
-        if (!config.contains("punishments.flood.threshold")) config.set("punishments.flood.threshold", 5);
-        if (!config.contains("punishments.flood.command")) config.set("punishments.flood.command", "tempmute %player% 15m Flood #%punish_id%");
-
-        try {
-            config.save(new File(getDataFolder(), "config.yml"));
-        } catch (IOException e) {
-            getLogger().warning("Could not save config.yml!");
-        }
+        return blockedWordsConfig;
     }
 
-    private void checkAndUpdateLanguageFiles() {
-        File languageFolder = new File(getDataFolder(), "languages");
-        File[] languageFiles = {
-                new File(languageFolder, "Locale_EN.yml"),
-                new File(languageFolder, "Locale_CZ.yml"),
-                new File(languageFolder, "Locale_DE.yml")
-        };
-
-        String defaultCzechMessages =
-                "prefix: \"§6AntiSpam §8» \"\n" +
-                        "blocked-word-message: \"§7Vaše zpráva obsahuje blokované slovo.\"\n" +
-                        "reload-message: \"§aPlugin byl úspěšně znovu načten.\"\n" +
-                        "wait-message: \"§7Musíte počkat §6%remaining_time% §7sekundy před odesláním další zprávy.\"\n" +
-                        "repeat-message: \"§7Neposílejte stejnou zprávu dvakrát za sebou.\"\n" +
-                        "no-permission-message: \"§cNemáte oprávnění k použití tohoto příkazu.\"\n" +
-                        "repeat-character-message: \"§7Vaše zpráva obsahuje příliš mnoho opakujících se znaků.\"\n" +
-                        "invalid-id-message: \"§cID musí být číslo.\"\n" +
-                        "punish-check-found: |-\n" +
-                        "  §7Vyhledávání trestu\n" +
-                        "  §7Punishment ID §6%id%\n" +
-                        "  §7player §e%player%\n" +
-                        "  §7category §c%category%\n" +
-                        "  §7for message: §f%content%\n" +
-                        "punish-check-not-found: \"§cTrest s tímto ID nebyl nalezen.\"\n";
-
-        String defaultGermanMessages =
-                "prefix: \"§6AntiSpam §8» \"\n" +
-                        "blocked-word-message: \"§7Ihre Nachricht enthält ein blockiertes Wort.\"\n" +
-                        "reload-message: \"§7Plugin wurde erfolgreich neu geladen.\"\n" +
-                        "wait-message: \"§7Sie müssen noch §6%remaining_time% §7Sekunden warten, bevor Sie eine weitere Nachricht senden können.\"\n" +
-                        "repeat-message: \"§7Senden Sie nicht zweimal hintereinander die gleiche Nachricht.\"\n" +
-                        "no-permission-message: \"§cSie haben keine Berechtigung, diesen Befehl zu verwenden.\"\n" +
-                        "repeat-character-message: \"§7Ihre Nachricht enthält zu viele wiederholte Zeichen.\"\n" +
-                        "invalid-id-message: \"§cID muss eine Nummer sein.\"\n" +
-                        "punish-check-found: |-\n" +
-                        "  §7Punishment Lookup\n" +
-                        "  §7Punishment ID §6%id%\n" +
-                        "  §7player §e%player%\n" +
-                        "  §7category §c%category%\n" +
-                        "  §7for message: §f%content%\n" +
-                        "punish-check-not-found: \"§cStrafe mit dieser ID nicht gefunden.\"\n";
-
-        String defaultEnglishMessages =
-                "prefix: \"§6AntiSpam §8» \"\n" +
-                        "blocked-word-message: \"§7Your message contains a blocked word.\"\n" +
-                        "reload-message: \"§7Plugin reloaded successfully.\"\n" +
-                        "wait-message: \"§7You need to wait §6%remaining_time% §7seconds before sending another message.\"\n" +
-                        "repeat-message: \"§7Do not send the same message twice in a row.\"\n" +
-                        "no-permission-message: \"§cYou do not have permission to use this command.\"\n" +
-                        "repeat-character-message: \"§7Your message contains too many repeating characters.\"\n" +
-                        "invalid-id-message: \"§cID must be a number.\"\n" +
-                        "punish-check-found: |-\n" +
-                        "  §7Punishment Lookup\n" +
-                        "  §7Punishment ID §6%id%\n" +
-                        "  §7player §e%player%\n" +
-                        "  §7category §c%category%\n" +
-                        "  §7for message: §f%content%\n" +
-                        "punish-check-not-found: \"§cPunishment with this ID not found.\"\n";
-
-        for (File file : languageFiles) {
-            FileConfiguration langConfig = YamlConfiguration.loadConfiguration(file);
-            String language = file.getName().split("_")[1].split("\\.")[0];
-
-            if (!langConfig.contains("prefix")) langConfig.set("prefix", getDefaultMessage(language, "prefix", defaultCzechMessages, defaultGermanMessages, defaultEnglishMessages));
-            if (!langConfig.contains("blocked-word-message")) langConfig.set("blocked-word-message", getDefaultMessage(language, "blocked-word-message", defaultCzechMessages, defaultGermanMessages, defaultEnglishMessages));
-            if (!langConfig.contains("reload-message")) langConfig.set("reload-message", getDefaultMessage(language, "reload-message", defaultCzechMessages, defaultGermanMessages, defaultEnglishMessages));
-            if (!langConfig.contains("wait-message")) langConfig.set("wait-message", getDefaultMessage(language, "wait-message", defaultCzechMessages, defaultGermanMessages, defaultEnglishMessages));
-            if (!langConfig.contains("repeat-message")) langConfig.set("repeat-message", getDefaultMessage(language, "repeat-message", defaultCzechMessages, defaultGermanMessages, defaultEnglishMessages));
-            if (!langConfig.contains("no-permission-message")) langConfig.set("no-permission-message", getDefaultMessage(language, "no-permission-message", defaultCzechMessages, defaultGermanMessages, defaultEnglishMessages));
-            if (!langConfig.contains("repeat-character-message")) langConfig.set("repeat-character-message", getDefaultMessage(language, "repeat-character-message", defaultCzechMessages, defaultGermanMessages, defaultEnglishMessages));
-            if (!langConfig.contains("invalid-id-message")) langConfig.set("invalid-id-message", getDefaultMessage(language, "invalid-id-message", defaultCzechMessages, defaultGermanMessages, defaultEnglishMessages));
-
-            if (!langConfig.contains("punish-check-found")) {
-                if (language.equals("CZ")) {
-                    langConfig.set("punish-check-found", "§7Vyhledávání trestu\n§7Punishment ID §6%id%\n§7player §e%player%\n§7category §c%category%\n§7for message: §f%content%");
-                } else {
-                    langConfig.set("punish-check-found", "§7Punishment Lookup\n§7Punishment ID §6%id%\n§7player §e%player%\n§7category §c%category%\n§7for message: §f%content%");
+    private void saveDefaultBlockedWords() {
+        if (blockedWordsFile == null) {
+            blockedWordsFile = new File(getDataFolder(), "BlockedWords.yml");
+        }
+        if (!blockedWordsFile.exists()) {
+            try {
+                saveResource("BlockedWords.yml", false);
+            } catch (IllegalArgumentException e) {
+                blockedWordsConfig = YamlConfiguration.loadConfiguration(blockedWordsFile);
+                blockedWordsConfig.set("blocked-words", java.util.Arrays.asList("badword1", "badword2"));
+                try {
+                    blockedWordsConfig.save(blockedWordsFile);
+                } catch (IOException ex) {
+                    getLogger().warning("Could not save BlockedWords.yml!");
                 }
             }
-            if (!langConfig.contains("punish-check-not-found")) langConfig.set("punish-check-not-found", getDefaultMessage(language, "punish-check-not-found", defaultCzechMessages, defaultGermanMessages, defaultEnglishMessages));
-
-            try {
-                langConfig.save(file);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
         }
+        reloadBlockedWordsConfig();
     }
 
-    private String getDefaultMessage(String language, String key, String defaultCzech, String defaultGerman, String defaultEnglish) {
-        String source = defaultEnglish;
-        if (language.equals("CZ")) source = defaultCzech;
-        if (language.equals("DE")) source = defaultGerman;
-
-        for (String line : source.split("\n")) {
-            if (line.startsWith(key + ":")) {
-                if (line.contains("|-")) return null;
-                return line.substring(line.indexOf("\"") + 1, line.lastIndexOf("\""));
+    private void saveDefaultLanguageFiles() {
+        String[] langs = {"EN", "CZ", "DE"};
+        for (String lang : langs) {
+            String resourcePath = "languages/Locale_" + lang + ".yml";
+            File file = new File(getDataFolder(), resourcePath);
+            if (!file.exists()) {
+                try {
+                    saveResource(resourcePath, false);
+                } catch (IllegalArgumentException e) {
+                    getLogger().warning("Default language file not found in resources: " + resourcePath);
+                }
             }
         }
-        return "";
     }
 }
